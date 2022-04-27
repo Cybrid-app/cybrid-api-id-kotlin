4ae5fc20969c770cd78861b0a4c679b248e89490
brent-cybrid <brent@cybrid.app>
2022-04-27 19:53:03 +0000

    v0.1.3

---
+++ src/main/kotlin/app/cybrid/cybrid_api_id/client/infrastructure/RequestMethod.kt
@@ -0,0 +1,8 @@
+package app.cybrid.cybrid_api_id.client.infrastructure
+
+/**
+ * Provides enumerated HTTP verbs
+ */
+enum class RequestMethod {
+    GET, DELETE, HEAD, OPTIONS, PATCH, POST, PUT
+}
